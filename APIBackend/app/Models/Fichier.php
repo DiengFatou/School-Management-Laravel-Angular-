8777eb3d7e853dd6373e448a987cdf260241f7ad
Limatou4 <996220.lima@gmail.com>
2025-07-21 19:08:51 +0000

    Mise à jour backend : model

--- APIBackend/app/Models/Fichier.php
+++ APIBackend/app/Models/Fichier.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Fichier extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nom_du_fichier',
+        'path',
+        'extension',
+        'taille',
+    ];
 }
